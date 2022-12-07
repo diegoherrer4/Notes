42beab3568e7683fecceb90dc296cfdac95e4026
Diego Herrera <dagomessis@gmail.com>
2022-12-07 08:45:25 -0500

    u

--- SimplePigLatin.js
+++
@@ -1,7 +0,0 @@
-function pigIt(str) {
-    var arrayWord = str.split(' ');
-    return arrayWord.map(function(word) {
-      var firstLetter = word.charAt(0);
-      return word.slice(1) + firstLetter + 'ay';
-    }).join(' ');
-  }
